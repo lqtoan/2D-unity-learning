21169fc33ca3a52e348d31d34da04f787e2a2e16
lqtoan <lqtoan37@gmail.com>
2024-09-13 10:35:05 +0700

    update tilemap

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     private bool canDoubleJump;
     private float lastDashTime;
 
-    private float threshold = -5f;
+    private float threshold = 10f;
 
     private void Awake()
     {
